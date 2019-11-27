c4400be9296e960fb10ac442dde51ede2d7ee612
Minchul Kang <tegongkang@gmail.com>
2019-11-28 00:47:31 +0900

    add UserBook, Product model

--- bookstore/admin.py
+++ bookstore/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import UserBook, Product
 # Register your models here.
+
+admin.site.register(UserBook)
+admin.site.register(Product)
--- bookstore/models.py
+++ bookstore/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from accounts.models import MyUser
 
 class UserBook(models.Model):
     title = models.CharField(max_length=255)
@@ -6,13 +7,40 @@ class UserBook(models.Model):
     link = models.URLField()
 
     author = models.CharField(max_length=200)
-    og_price = models.CharField(max_length=200)
+    price = models.CharField(max_length=200)
 
     publisher = models.CharField(max_length=200)
     pub_date = models.CharField(max_length=200)
-    isbn = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=200, primary_key=True)
 
     description = models.CharField(max_length=500)
 
     def __str__(self):
         return self.title
+
+
+class Product(models.Model):
+    
+    exchange_type = (
+        ("양도", "양도"),
+        ("대여", "대여"),
+        ("판매", "판매")
+    )
+
+    book_state = (
+        ("unavailable", "unavailable"),
+        ("available", "available"),
+    )
+
+    exchange_method = (
+        ("일반거래", "일반거래"),
+        ("안심거래", "안심거래"),
+    )
+
+    user = models.ForeignKey(MyUser, on_delete = models.CASCADE, null=True)
+    book = models.ForeignKey(UserBook, on_delete = models.CASCADE, null=True)
+    note = models.CharField(max_length=255)
+    ex_type = models.CharField(max_length=10, choices=exchange_type, default="양도")               # 대여, 양도, 판대
+    state = models.CharField(max_length=10, choices=book_state, default="unavailable")                  # 판매 완료
+    exc_method = models.CharField(max_length=10, choices=exchange_method, default="일반거래")             # 안심거래, 일반거래 
+    price = models.CharField(max_length=100, blank=True)
